d4e1f6888b0e157f2582017442ddcf19d46ddd18
mozturan <mehmetozturan@proton.me>
2026-02-08 22:09:06 +0300

    wrapper class for gymnasium envs

---
+++ wrapper.py
@@ -0,0 +1,63 @@
+from typing import Any
+import gymnasium as gym
+import numpy as np
+from abc import ABC
+
+class Wrapper(ABC, gym.Wrapper):
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
+        return super().reset(seed=seed, options=options)
+
+    def step(self, action: Any) -> tuple[Any, float, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = super().step(action)
+        return obs, float(reward), terminated, truncated, info
+    
+class SACHighwayWrapper(Wrapper):
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+
+        # self.config = config or {}
+        
+        # Initialize the environment with the given configuration
+        #! In this case i can't set new obs shape correctly idk why
+        # self._init_env()
+
+        
+        original_obs_shape = env.observation_space.shape
+        if isinstance(original_obs_shape, tuple):
+            new_obs_shape = (int(np.prod(original_obs_shape)) + 1,) # Flatten the original observation and add 1 dimension for speed
+            self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=new_obs_shape, dtype=np.float32)
+        else:
+            raise ValueError("env.observation_space.shape is not a tuple. Make sure your environment uses continuous states.")
+                                
+    # def _init_env(self):
+    #     # Update the environment's configuration with the provided config
+    #     config = getattr(self.env.unwrapped, 'config', None)
+    #     if config is not None:
+    #         config.update(self.config)
+    #     else:
+    #         raise ValueError("The environment does not have a 'config' attribute. Make sure your environment is compatible with this wrapper.")
+
+    def step(self, action: Any) -> tuple[Any, float, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        return self._observation(obs, info), float(reward), terminated, truncated, info
+    
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
+        state, info = super().reset(seed=seed, options=options)
+        return self._observation(state, info), info
+    
+    def _getinfo(self, info: dict[str, Any]) -> np.ndarray:
+        return np.array(float(info["speed"]))
+
+    def _observation(self, obs: Any, info: Any) -> np.ndarray:
+        
+        "! This function takes the original observation and info, "
+        "flattens the observation, extracts the speed from info, "
+        "and concatenates them into a single observation array."
+
+        info = self._getinfo(info)
+        obs = obs.flatten() if isinstance(obs, np.ndarray) else np.array(obs).flatten()
+        obs = np.concatenate((obs, [info]), axis=0)
+        return obs
